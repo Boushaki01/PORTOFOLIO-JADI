fb0e7a93b72309522a5b034c7288aa433fd86ca1
Kiki Baihaki <75324728+Boushaki01@users.noreply.github.com>
2021-04-11 12:35:52 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// deklarasi kotak email
+var modal = document.getElementById('modal-box-email');
+
+// fungsi saat kotak email dibuka
+function modalShow() {
+    modal.style.display = 'flex';
+}
+// fungsi saat kotak email ditutup
+function modalClose() {
+    modal.style.display = 'none';
+}
